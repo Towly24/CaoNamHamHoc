bc969cbe442cb8fd834f6478dc3e26a744bb7dad
towly249 <caonamtq249@gmail.com>
2022-09-20 17:36:03 +0700

    17/9

--- src/assignment2/Account.java
+++ src/assignment2/Account.java
@@ -5,37 +5,61 @@ import java.util.Scanner;
 public class Account {
     int id;
     String name;
-    int balance,amount;
+    int balance;
     public Account () {
         this.id = id;
         this.name = name;
         this.balance = balance;
-        this.amount = amount;
+
     }
     public Account (int id, String name, int balance) {
         this.id = id;
         this.name = name;
         this.balance = balance;
-        this.amount = amount;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
     public void Credit() {
         Scanner cd = new Scanner(System.in);
-        System.out.println("Nạp tiền vào tài khoản: ");
-        amount = cd.nextInt();
+        System.out.println("Nạp tiền vào tài khoản với " +id +": ");
+        int amount = cd.nextInt();
         if(amount>0){
-            balance+=amount;
+            this.balance+=amount;
         }
-        System.out.println("Tài khoản hiện tại có: " +balance);
+        System.out.println("Tài khoản "+ id +" hiện tại có " +balance);
     }
     public void Debit() {
         Scanner cd = new Scanner(System.in);
         System.out.println("Thanh toán tiền: ");
-        amount = cd.nextInt();
+        int amount = cd.nextInt();
         if(amount>balance){
             System.out.println("thanh toán không thành công");
         }
         else{
-            balance-=amount;
+            this.balance-=amount;
             System.out.println("Tài khoản còn lại: "+balance);
         }
     }
